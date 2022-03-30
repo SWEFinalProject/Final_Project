8eb9eb36daa9a8abe06ffc32bf68658cb6e97776
root <root@LAPTOP-IUI6PNV0.localdomain>
2022-03-29 23:43:40 -0400

    Deployment problem fixed

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
-
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -12,3 +11,4 @@ app.run(
     port=int(os.getenv('PORT', 8080)),
     debug=True
 )
+
