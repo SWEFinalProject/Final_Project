c66e34604e54dd3b928cc3563f6719e8f53b6b0b
root <root@LAPTOP-IUI6PNV0.localdomain>
2022-04-08 23:52:15 -0400

    Added modification to the linking for the comments page

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,9 +1,11 @@
 import { React, useState, useEffect } from "react";
 import { Comment, Groups, GroupComment } from "./styles/comments.style";
 import { GroupTab, Labels } from "./styles/GroupTab.style";
+import { CommentBox } from "./styles/commentBox.style";
 
 function Comments() {
   const [state, setState] = useState("");
+  const [comment, setCurrentComment] = useState("");
   function fetchAPI() {
     fetch("/api")
       .then((response) => {
@@ -38,10 +40,15 @@ function Comments() {
       </Groups>
 
       <GroupComment>
-        <form action="">
-          <input type="text" />
+        <h3>{comment}</h3>
+        <CommentBox action="">
+          <input
+            type="text"
+            required
+            onChange={(e) => setCurrentComment(e.target.value)}
+          ></input>
           <input type="submit" />
-        </form>
+        </CommentBox>
       </GroupComment>
     </Comment>
   );
