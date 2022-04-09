7416bd45f5ee6d252418e1d7112785069eb55968
Alina <anemira@Alinas-MBP.wifi.gsu.edu>
2022-04-08 20:10:10 -0400

    Alina Nemira

---
+++ api_setup.py
@@ -0,0 +1,11 @@
+"""This module is an API set up
+SHOULD WORK WITH YELP API"""
+import os
+import random
+import requests
+import sys
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+
