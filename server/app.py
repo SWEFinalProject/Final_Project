f14c2d9115bff665fb1e9e0222d22adb3ae8fea6
root <root@LAPTOP-IUI6PNV0.localdomain>
2022-04-18 14:40:54 -0400

    Added socketIO to chat. more features will be added soon

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+"""Backend"""
 import os
 import flask
 from model import Users, Restaurant, Chatroom, Ct
@@ -8,9 +9,8 @@ import flask_login as fl
 import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import ApplicationConfig
+from flask_socketio import SocketIO, send
 
-
-"""FLASK-login manager"""
 app = flask.Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
@@ -29,6 +29,8 @@ if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
 login_manager = fl.LoginManager(app)
 login_manager.login_view = "login"
 login_manager.init_app(app)
+socketIo = SocketIO(app, cors_allowed_origins="*")
+# app.debug = True
 
 
 bp = flask.Blueprint(
@@ -37,107 +39,111 @@ bp = flask.Blueprint(
     template_folder="./static/react",
 )
 
-"""Database classes + table"""
-
 
 @app.route("/get_all_restaurants",methods=["GET"])
 def get_all_restaurants():
-    rest_list = db.engine.execute(f""" select * from "restaurant"  """).all()
-    pass
+    """Database classes + table"""
+    # rest_list = db.engine.execute(f""" select * from "restaurant"  """).all()
     #rest_name = [rest1, rest2, ...]
 
 @app.route("/get_business_data/<name>",methods=["GET"]) # name is a value (restaurant's name)
 def get_business_data(name):
     """Usage: localhost:5000/get_business_data/cafe lucia """
     return get_data(name)
-   
 
 @app.route("/", methods=["POST", "GET"])
 def login():
+    """Login"""
     if flask.request.method == "POST":
         username = flask.request.json["username"]
-        pd = flask.request.json["password"]
-        pd_hash = hashlib.md5(pd.encode("utf-8")).hexdigest()
-        isUser = user.query.filter_by(username=username).first()
-        if not isUser:
+        p_d = flask.request.json["password"]
+        pd_hash = hashlib.md5(p_d.encode("utf-8")).hexdigest()
+        is_user = user.query.filter_by(username=username).first()
+        if not is_user:
             return flask.jsonify({"error": "Not found"}), 404
-        if not check_password_hash(isUser.password, pd_hash):
+        if not check_password_hash(is_user.password, pd_hash):
             return flask.jsonify({"error": "Unathorized"}), 401
-        return flask.jsonify({"id": isUser.id, "username": isUser.username})
+        return flask.jsonify({"id": is_user.id, "username": is_user.username})
     return flask.jsonify("Not post request")
 
 
 @login_manager.user_loader
 def load_user(user_id):
     """Load user for login manager"""
-    return User.query.get(int(user_id))
+    return Users.query.get(int(user_id))
 
-
-"""PLACEHOLDER for the route to the main page"""
+@socketIo.on("message")
+def handleMessage(msg):
+    print(msg)
+    send(msg, broadcast=True)
 
 
 @app.route("/pmain", methods=["POST", "GET"])  # was /loggeduser
 @fl.login_required
 def main():
+    """Not sure what this is doing"""
     if flask.request.method == "POST":
         # if flask.request.json.get("action") == "profile":
         #     return flask.redirect("/profile")
         # elif flask.request.json.get("action") == "submit":
         #     # funtion call to handle functionality for submit action(similar to else below)
         #     pass
-        review_page(flask.request.form)
+        # review_page(flask.request.form) # what is this?
         return flask.redirect("/on_submit_button")
 
-    else:
-        """This function creating a template using .html file
-        and pass values to the variables in a template"""
-        name, tagline, genres, poster_path, mov_url, movie_id = get_data()
-        base_url, poster_sizes = get_config()
-        review = db.engine.execute(
-            f""" select * from "review" where movie_id = '{movie_id}' """
-        ).all()
-        print(review)
-        flask.flash(review)
-        return flask.render_template(
-            ["index.html"],
-            name=name,
-            tagline=tagline,
-            genres=genres,
-            poster_path=poster_path,
-            base_url=base_url,
-            poster_sizes=poster_sizes,
-            url=base_url + poster_sizes + poster_path,
-            movie_url=mov_url,
-            movie_id=movie_id,
-        )
+    # This function creating a template using .html file
+    # and pass values to the variables in a template
+    ## failing the pylint
+    # pylint: disable=no-value-for-parameter
+    # pylint: disable=unpacking-non-sequence
+    # pylint: disable=undefined-variable
+    name, tagline, genres, poster_path, mov_url, movie_id = get_data()
+    base_url, poster_sizes =  get_config()
+    review = db.engine.execute(
+        f""" select * from "review" where movie_id = '{movie_id}' """
+    ).all()
+    print(review)
+    flask.flash(review)
+    return flask.render_template(
+        ["index.html"],
+        name=name,
+        tagline=tagline,
+        genres=genres,
+        poster_path=poster_path,
+        base_url=base_url,
+        poster_sizes=poster_sizes,
+        url=base_url + poster_sizes + poster_path,
+        movie_url=mov_url,
+            ovie_id=movie_id,
+    )
 
 
-"""PLACEHOLDER for the route to the restaurant page
-~coming from a SEARCH bar"""
+
 
 
 @app.route("/restaurant", methods=["POST", "GET"])
 @fl.login_required
 def restaurant():
+    """PLACEHOLDER for the route to the restaurant page coming from a SEARCH bar"""
     return flask.render_template("restaurant.html")
 
 
-"""PLACEHOLDER for the route to the profile page
-~coming from a button on main page"""
+# PLACEHOLDER for the route to the profile page coming from a button on main page
 
 
 @app.route("/profile", methods=["POST", "GET"])
 def profile():
+    """Profile"""
     return flask.render_template("profile.html")
 
 
-"""PLACEHOLDER for the route to the chat page (REACT)
-~coming from a button
-SHOULD BE A REACT PAGE"""
 # route for serving React page
 @bp.route("/")
 @fl.login_required
 def index():
+    """PLACEHOLDER for the route to the chat page (REACT)
+    ~coming from a button
+    SHOULD BE A REACT PAGE"""
     # NB: DO NOT add an "index.html" file in your normal templates folder
     # Flask will stop serving this React page correctly
     return flask.render_template("index.html")
@@ -147,6 +153,7 @@ def index():
 @bp.route("/chat", methods=["POST"])
 @fl.login_required
 def chat():  # should return a JSON file with a fun fact in it
+    """Chat"""
     chats = ["Hi there! It is our chat page"]
     return flask.jsonify(chats)
 
@@ -156,6 +163,7 @@ def chat():  # should return a JSON file with a fun fact in it
 def register():
     """registration"""
     # pylint: disable=no-member
+    # pylint: disable=unused-variable
     if flask.request.method == "POST":
         f_name =  flask.request.json["f_name"]
         l_name =  flask.request.json["l_name"]
@@ -168,7 +176,7 @@ def register():
         hashed_password = generate_password_hash(password, method="sha256")
         user_exists = Users.query.filter_by(gsu_id=gsu_id).first()
         if user_exists:
-             return flask.jsonify({"error": "Unauthorized"}), 401
+            return flask.jsonify({"error": "Unauthorized"}), 401
 
         new_user = Users(
             f_name=f_name,
@@ -191,10 +199,9 @@ app.register_blueprint(bp)
 
 
 if __name__ == "__main__":
-    app.run(
+    socketIo.run(app,
         host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
     )
-    
 # {
 #     "f_name" : "Nur",
 #     "l_name": "Haque",
